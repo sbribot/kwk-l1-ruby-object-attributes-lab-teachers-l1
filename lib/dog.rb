0dddf6b543f09b50c40ee791254267b68f1330d1
Sara Beth Ribot <sbribot@gmail.com>
2018-06-07 03:15:45 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,19 @@
 # dog.rb
+class Dog 
+  def name=(name)
+    @dog_name = name 
+  end 
+  
+  def name 
+    @dog_name
+  end
+  
+  def breed=(some_breed)
+    @breed = some_breed
+  end 
+  
+  def breed
+    @breed
+  end 
+  
+  

023929a76146f40633c325e2d66581f7ed75ec46
Sara Beth Ribot <sbribot@gmail.com>
2018-06-07 03:20:31 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,21 @@
 # dog.rb
+class Dog 
+  def name=(name)
+    @name = name 
+  end 
+  
+  def name 
+    @name
+  end
+  
+  def breed=(some_breed)
+    @breed = some_breed
+  end 
+  
+  def breed
+    @breed
+  end 
+end 
+
+
+  
--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,20 @@
 # person.rb
+
+class Person  
+  def name=(person_name)
+    @name = person_name
+  end
+  
+  def name
+    @name
+  end
+  
+  def job=(some_job)
+    @job = some_job
+  end
+  
+  def job
+    @job
+  end
+  
+end 
